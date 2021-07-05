d0708ebac229ce9095ff8229100372975bf92e20
q969210177 <969210177@qq.com>
2021-07-05 10:40:25 +0800

    修改一下模板

--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -1,24 +1,15 @@
-import { defineComponent,h } from 'vue'
-import ttt from './test'
+import { defineComponent } from 'vue';
 import { Vue } from 'vue-class-component'
-import './index.scss'
-interface yyy  extends Vue{
-  text:string | number
-  rule:any[]
+interface index  extends Vue{
+
 }
-const index = defineComponent({
-  name: 'Index',
-  components: {
-  },
-  setup() {
-    const rule = [{ type: 'a-button', props:{ text:222 } } ]
-    return {
-      rule,
-    }
-  },
-  render(vm: yyy) {
-    // <a-button value={111}>111</a-button>
-    return <div class="aaa">{h(ttt,{ type:'primary'  },[111])}</div>
-  },
+const  index = defineComponent({
+ name:'index',
+ setup(){
+  return{}
+ },
+ render(vm: index) {
+  return <div class='index'></div>
+ },
 })
 export default index
