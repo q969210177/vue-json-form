f10e731ba5ac17c9f591833e2ba696101dc71c14
q969210177 <969210177@qq.com>
2021-07-05 09:34:10 +0800

    测试提交

--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -1,13 +1,11 @@
 import { defineComponent,h } from 'vue'
 import ttt from './test'
-import el from '@/form/index'
 import { Vue } from 'vue-class-component'
 import './index.scss'
 interface yyy  extends Vue{
   text:string | number
   rule:any[]
 }
-el.user('aaaa',ttt)
 const index = defineComponent({
   name: 'Index',
   components: {
@@ -19,7 +17,8 @@ const index = defineComponent({
     }
   },
   render(vm: yyy) {
-    return <div class="aaa">{h('a-input',{ value:111 ,placeholder:'3333' })}</div>
+    // <a-button value={111}>111</a-button>
+    return <div class="aaa">{h(ttt,{ type:'primary'  },[111])}</div>
   },
 })
 export default index
--- src/views/index/test.tsx
+++ src/views/index/test.tsx
@@ -1,9 +1,17 @@
 import { defineComponent } from 'vue';
-import el from '@/form/index'
 const  test = defineComponent({
   name:'Test',
+  props:{
+    type:{
+      type:[String,Number],
+      default:'',
+    },
+  },
   setup(){
     return{}
+  },
+  render(vm:any){
+    return <div>{vm.type}</div>
   },
 })
 export default test
