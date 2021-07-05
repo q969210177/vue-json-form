3b04826d4e3557274ff4c6158f5a1ea5e2a4b017
q969210177 <969210177@qq.com>
2021-07-05 17:48:57 +0800

    实现一下 v-model

--- src/form/corsrc/CForm.tsx
+++ src/form/corsrc/CForm.tsx
@@ -1,26 +1,32 @@
 import { defineComponent } from 'vue';
-import { Vue } from 'vue-class-component'
-import { getRuleItem } from '@/form/utils/getRuleItem'
+import { Vue } from 'vue-class-component';
+import { getRuleItem } from '@/form/utils/getRuleItem';
 import { IRuleItem } from '../types/ruleType';
-interface CForm  extends Vue{
-    rule:[]
-    formModel:{[x:string]:any;}
+interface CForm extends Vue {
+  rule: []
+  formModel: { [x: string]: any; }
 }
-const  CForm = defineComponent({
-  name:'CForm',
-  props:{
-    rule:{
-      type:Array,
-      default:()=>{
-        return []
+const CForm = defineComponent({
+  name: 'CForm',
+  props: {
+    rule: {
+      type: Array,
+      default: () => {
+        return [];
       },
     },
   },
-  setup(){
-    const formModel = {}
-    return{
-      formModel,
+  setup(props) {
+    const formModel: CForm['formModel'] = {};
+    if (props.rule.length) {
+      const rule: IRuleItem[] = props.rule as IRuleItem[];
+      rule.forEach((i: IRuleItem) => {
+        formModel[i.name] = i.value;
+      });
     }
+    return {
+      formModel,
+    };
   },
   /**
    *
@@ -28,13 +34,20 @@ const  CForm = defineComponent({
    * @returns
    */
   render(vm: CForm) {
-    return <div class='CForm'>
-      <a-form model={vm.formModel}>
-        {vm.rule.map((i:IRuleItem)=>{
-          return <a-form-item label="Activity name">{getRuleItem(i)}</a-form-item>
-        })}
-      </a-form>
-    </div>
+    console.log(vm.formModel, '1111');
+    return (
+      <div class="CForm">
+        <a-form model={vm.formModel}>
+          {vm.rule.map((i: IRuleItem) => {
+            return (
+              <a-form-item label="Activity name">
+                {getRuleItem(i, vm)}
+              </a-form-item>
+            );
+          })}
+        </a-form>
+      </div>
+    );
   },
-})
-export default CForm
+});
+export default CForm;
--- src/views/index/index.tsx
+++ src/views/index/index.tsx
@@ -1,27 +1,32 @@
 import { IRuleItem } from '@/form/types/ruleType';
-import {  defineComponent, ref } from 'vue';
-import { Vue } from 'vue-class-component'
-import test from './components/test'
-import el from '@/form/index'
-el.component(test.name,test)
-interface index  extends Vue{
-  testClick:()=>void
-  count:number
+import { defineComponent, ref } from 'vue';
+import { Vue } from 'vue-class-component';
+import test from './components/test';
+import el from '@/form/index';
+el.component(test.name, test);
+interface index extends Vue {
+  testClick: () => void
+  count: number
+  value: string | number
 }
-const  index = defineComponent({
-  name:'Index',
-  setup(){
-    return{   }
+const index = defineComponent({
+  name: 'Index',
+  setup() {
+    const value = ref(0);
+    return { value };
   },
   render(vm: index) {
-    const rule:IRuleItem[] = [
-      { type:'DatePicker',name:'aaaa',value:'' },
-      { type:'Input',name:'cccc',value:'' },
-      { type:'Test',name:'cccc',value:'' },
-    ]
-    return <div class='index' >
-      <c-form rule={rule}></c-form>
-    </div>
+    const rule: IRuleItem[] = [
+      { type: 'DatePicker', name: 'aaaa', value: '2020-01-01' },
+      { type: 'Input', name: 'cccc', value: '111111' },
+      { type: 'Test', name: 'zzzz', value: '' },
+    ];
+    return (
+      <div class="index">
+        <c-form rule={rule}></c-form>
+        <a-textarea value={vm.value} placeholder="Basic usage" />
+      </div>
+    );
   },
-})
-export default index
+});
+export default index;
